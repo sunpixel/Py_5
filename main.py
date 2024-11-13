1093aacab736dc0dcad1fd1758d85c96a33aa0ff
SunPixel <tarakinnikita@gmail.com>
2024-11-13 17:28:46 +0300

    More updates

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def func_8():
     mid = len(x) // 2
     mid = x[mid]
     i = x.count(mid)
-    return i
+    return i, mid, x
 
 def func_9(*args, size = 15):
     '''Just a function'''
@@ -128,15 +128,116 @@ def func_9(*args, size = 15):
         x.pop()
     x.sort()
     return x
-
+# Needs to be done ASAP
 def func_10():
     '''Just a function'''
     print('=' * 60)
+    def funct_1():
+        '''Just a function'''
+        print('=' * 60)
+    def funct_2():
+        '''Just a function'''
+        print('=' * 60)
+    def funct_3():
+        '''Just a function'''
+        print('=' * 60)
+    def funct_4():
+        '''Just a function'''
+        print('=' * 60)
+    def funct_5():
+        '''Just a function'''
+        print('=' * 60)
+    def funct_6():
+        '''Just a function'''
+        print('=' * 60)
+    funct_1()
+    funct_2()
+    funct_3()
+    funct_4()
+    funct_5()
+    funct_6()
+
+def func_11(args):
+    '''Just a function'''
+    print('=' * 60)
+    return args.pop(args.index(min(args))) , args
+
+def func_12(*args):
+    '''Just a function'''
+    print('=' * 60)
+    return list(map(lambda x: x * x, args))
+
+def func_13(*args):
+    '''Just a function'''
+    print('=' * 60)
+    return list(map(lambda x: type(x), args))
+
+def func_14(*args, elem):
+    '''Just a function'''
+    print('=' * 60)
+    if args.count(elem) != 0 and args.count(elem) > 0:
+        return True
+    return False
+
+def func_15(*args):
+    '''Just a function'''
+    print('=' * 60)
+    return list(map(lambda x: x, args))
+
+def func_16(*args):
+    '''Just a function'''
+    print('=' * 60)
+    def do_1(dicto):
+        '''Get random data from dictionary'''
+        x = list(map(lambda x: x, dicto.keys()))
+        y = x[r.randint(0, len(x) - 1)]
+        return f'{y} : {dicto.get(y)}'
+    def do_2(dicto):
+        '''Sort func'''
+        xy = []
+        x = list(map(lambda x: x, dicto))
+        x.sort()
+        for i in x:
+            vals = list()
+            s = dicto.get(i)
+            vals.append(i)
+            vals.append(s)
+            xy += vals
+        return xy
+    def do_3(dicto):
+        '''Sort by value'''
+        return sorted(dicto.items(), key=lambda x: x[1])
+
+    return do_1(args[0]), do_2(args[1]), do_3(args[2])
+
 
 if __name__ == '__main__':
     this = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
     that = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
     those =[20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+    capitals = {
+    'Russia': 'Moscow',
+    'France': 'Paris',
+    'Germany': 'Berlin',
+    'Italy': 'Rome',
+    'Japan': 'Tokyo'
+    }
+    currency_rates = {
+    'USD': 60,
+    'EUR': 65,
+    'GBP': 70,
+    'JPY': 80,
+    'CHF': 90
+    }
+
+    prices = {
+    'Bread': 40,
+    'Milk': 35,
+    'Cheese': 45,
+    'Butter': 55,
+    'Yogurt': 20
+    }
 
     print(func_1(this))
     print(func_2([20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]))
@@ -147,3 +248,10 @@ if __name__ == '__main__':
     print(func_7(that, those, sort_descending=False))
     print(func_8())
     print(func_9(this, that, size= 15))
+
+    print(func_11(this))
+    print(func_12(1, 2, 3, 4, 5, 6))
+    print(func_13(1, '1', 3.02, 'Text', 5, 6))
+    print(func_14(1, 2, 3, 4, 5, 6, elem=6))
+    print(func_15(this, that, this))
+    print(func_16(capitals, currency_rates, prices))
